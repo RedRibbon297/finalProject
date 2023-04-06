430b3b588525acebf9f93744af27efafb0adb890
RedRibbon297 <anastasia_music@mail.ru>
2023-04-06 15:10:39 +0300

    home_commit_6

--- src/main/java/by/mysite/controllers/order/RemoveOrderController.java
+++ src/main/java/by/mysite/controllers/order/RemoveOrderController.java
@@ -1,16 +1,12 @@
 package by.mysite.controllers.order;
 
 import by.mysite.controllers.abstracts.AbstractOrderController;
-import by.mysite.model.entities.order.Order;
-import by.mysite.model.entities.users.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.List;
 
 import static by.mysite.constants.ApplicationConstant.ORDERS_HISTORY_CONTROLLER;
 import static by.mysite.constants.ApplicationConstant.REMOVE_ORDER_CONTROLLER;
